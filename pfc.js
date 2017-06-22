cf8ca9192187df21bb95b382935dd1ec8c77e470
Selbahc <selbahc@users.noreply.github.comm>
2017-06-22 16:58:03 +0200

    Premier code fonctionnel

--- pfc.js
+++ pfc.js
@@ -12,4 +12,27 @@ function getRanNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var computerNum = getRanNum(0, 100);
+var computerNum = getRanNum(1, 3);
+var userScore = 0;
+var computerScore = 0;
+var userNum;
+
+for (var i = 0; i < 3; i++) {
+  var userChoice = prompt("Choisissez entre Pierre, Feuille ou Ciseaux").toUpperCase();
+
+  switch (userChoice) {
+    case "CISEAUX":
+      userNum = 1;
+      break;
+    case "FEUILLE":
+      userNum = 2;
+      break;
+    case "PIERRE":
+      userNum = 3;
+      break;
+  }
+  userNum > computerNum ? userScore++ : userNum < computerNum ? computerScore++ : false;
+}
+
+alert(userScore > computerScore ? `Bravo, vous avez gagné avec ${userScore} points contre ${computerScore} !` : userScore == computerScore ? `ÉGALITÉ (${userScore} points chacun...)` : `Désolé, vous avez perdu avec ${userScore} points contre ${computerScore}`);
+alert(`Votre Score : ${userScore}\nScore de l'ordinateur : ${computerScore}`);
